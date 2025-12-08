0ca403414bbb5850a257613a43ace497d6e299fd
Ricardo Mantilla <jrmantillap@utn.edu.ec>
2025-12-08 16:52:45 -0500

    actualizacion foto perfil

---
+++ JavaScript/presentacion.js
@@ -0,0 +1,18 @@
+class IngenieroSoftware {
+    constructor(nombre) {
+        this.nombre = nombre || "Ricardo Mantilla";
+        this.rol = "Ingeniero de Software Jr";
+        this.experiencia = "En crecimiento";
+        this.pasion = "Creación de Soluciones Escalables";
+        this.enfoque = "Desarrollo Web";
+        this.disponibilidad = true;
+        this.curiosidad = Infinity;
+        this.skills = [
+            "Java (Spring Boot)", "JavaScript (Node.js)",
+            "TypeScript(NestJs)", "Angular & React",
+            "Microservicios (REST, GraphQL)",
+            "Bases de Datos (SQL, NoSQL)", "Git",
+            "Docker", "Metodologías Ágiles (Scrum, Kanban)"
+        ];
+    }
+}
